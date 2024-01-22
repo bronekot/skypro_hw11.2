6025b510e0404f827697c63012946f0a344376ac
BroneKot <13498924+bronekot@users.noreply.github.com>
2024-01-23 00:21:46 +0300

    update str_func.py and add func string_capitalize

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def string_upper(text: str):
+    """:return uppercase text"""
     return text.upper()
+
+
+def string_capitalize(text: str):
+    """:return capitalized text"""
+    return text.capitalize()
+
